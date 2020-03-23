463ac16853eb32bb93d9223e93d8c5f59f1617e0
DONATUS SILVA RICHARD <silva.donatus@gmail.com>
2020-03-22 22:35:21 -0400

    Updates

---
+++ precache-manifest.9b84c39e72f49b997d83b7aadaca266b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75f2b480942fa7077920cf22973458e9",
+    "url": "/donatusrichard.github.io/index.html"
+  },
+  {
+    "revision": "5e1c3aa48214b2e433ae",
+    "url": "/donatusrichard.github.io/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "597f1023b4ef9ffdc3e1",
+    "url": "/donatusrichard.github.io/static/css/main.28578a97.chunk.css"
+  },
+  {
+    "revision": "5e1c3aa48214b2e433ae",
+    "url": "/donatusrichard.github.io/static/js/2.3ee30c62.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/donatusrichard.github.io/static/js/2.3ee30c62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "597f1023b4ef9ffdc3e1",
+    "url": "/donatusrichard.github.io/static/js/main.95288d46.chunk.js"
+  },
+  {
+    "revision": "503d8ee152cd36e6c348",
+    "url": "/donatusrichard.github.io/static/js/runtime-main.8a0c682a.js"
+  },
+  {
+    "revision": "7133c479d287e7fc45e167857bb28e7f",
+    "url": "/donatusrichard.github.io/static/media/BackgroundGenerator.7133c479.JPG"
+  },
+  {
+    "revision": "84accfaf87b474d8a3bca2f5f74a8977",
+    "url": "/donatusrichard.github.io/static/media/Donatus Silva Richard-Website.84accfaf.pdf"
+  },
+  {
+    "revision": "41f74ff23a0ae066cb71e0702177207a",
+    "url": "/donatusrichard.github.io/static/media/DonatusImg.41f74ff2.jpg"
+  },
+  {
+    "revision": "556a0f10eddd9c308014e55ed1ffa3c3",
+    "url": "/donatusrichard.github.io/static/media/FaceDetectionApp.556a0f10.JPG"
+  },
+  {
+    "revision": "82ad5713b699eaeec173b96875ec893c",
+    "url": "/donatusrichard.github.io/static/media/FriendFinder.82ad5713.JPG"
+  }
+]);
